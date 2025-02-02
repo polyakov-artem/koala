8314345d8c14154e523dd744d9de2a1ba507db89
polyakov-artem <polyakov.contact@yandex.ru>
2025-02-02 10:39:06 +0300

    feat: add feature to Button to select base component

--- src/components/shared/Button/Button.tsx
+++ src/components/shared/Button/Button.tsx
@@ -1,6 +1,7 @@
-import './button.scss';
-import { ComponentProps, FC } from 'react';
+import { ComponentProps, FC, useMemo } from 'react';
 import { getClasses } from '../../../../tests/utils/getClasses';
+import { Link, LinkProps } from 'react-router';
+import './button.scss';
 
 export const BTN = 'btn';
 export const BTN_INNER = `${BTN}__inner`;
@@ -8,23 +9,50 @@ export const BTN_VIEW_PRIMARY = `${BTN}_view_primary`;
 export const BTN_THEME_PRIMARY = `${BTN}_theme_primary`;
 export const BTN_FULL_WIDTH = `${BTN}_full-width`;
 
-export type TButtonProps = ComponentProps<'button'> & {
+export type TButtonCustomProps = {
   theme?: 'primary';
   view?: 'primary';
   fullWidth?: boolean;
+  capital?: boolean;
 };
 
-const Button: FC<TButtonProps> = (props) => {
-  const { children, className, theme, view, fullWidth, ...restProps } = props;
-  const classes = getClasses(BTN, className, { theme, view, fullWidth });
+export type TButtonProps = (
+  | (ComponentProps<'button'> & { el?: 'button' })
+  | (LinkProps & { el?: 'link' })
+) &
+  TButtonCustomProps;
 
-  return (
-    <button className={classes} {...restProps}>
+const Button: FC<TButtonProps> = (props) => {
+  const {
+    className,
+    children,
+    theme,
+    view,
+    fullWidth,
+    capital,
+    el = 'button',
+    ...restProps
+  } = props;
+  const classes = getClasses(BTN, className, { theme, view, fullWidth, capital });
+
+  const inner = useMemo(
+    () => (
       <span className={BTN_INNER}>
         <span className={BTN_INNER}>{children}</span>
       </span>
-    </button>
+    ),
+    [children]
   );
+
+  const elProps = { className: classes, children: inner, ...restProps };
+
+  if (el === 'button') {
+    return <button {...(elProps as ComponentProps<'button'>)} />;
+  }
+
+  if (el === 'link') {
+    return <Link {...(elProps as LinkProps)} />;
+  }
 };
 
 export default Button;
