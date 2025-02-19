0b97ba7e217dc69e32e704324e8342aaf306ded9
polyakov-artem <polyakov.contact@yandex.ru>
2025-02-19 10:37:34 +0300

    feat: add Swiper to ProductGallery, refactor ProductGallery, add constant

--- src/components/ui/ProductGallery/ProductGallery.tsx
+++ src/components/ui/ProductGallery/ProductGallery.tsx
@@ -2,8 +2,10 @@ import { ComponentProps, FC, useCallback, useMemo, useState } from 'react';
 import classNames from 'classnames';
 import { TAppearance, TProduct } from '../../../types/types';
 import Button from '../../shared/Button/Button';
+import { IoIosArrowBack, IoIosArrowDown, IoIosArrowForward } from 'react-icons/io';
+import { Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import './ProductGallery.scss';
-import { IoIosArrowDown } from 'react-icons/io';
 
 export type TProductGalleryProps = ComponentProps<'div'> & {
   product: TProduct;
@@ -12,60 +14,98 @@ export type TProductGalleryProps = ComponentProps<'div'> & {
 
 export const PRODUCT_GALLERY = 'product-gallery';
 export const PRODUCT_GALLERY_GRID = `${PRODUCT_GALLERY}__grid`;
-export const PRODUCT_GALLERY_ITEM = `${PRODUCT_GALLERY}__item`;
+export const PRODUCT_GALLERY_MEDIA_ITEM = `${PRODUCT_GALLERY}__media-item`;
+export const PRODUCT_GALLERY_MEDIA = `${PRODUCT_GALLERY}__media`;
 export const PRODUCT_GALLERY_LOAD_BTN = `${PRODUCT_GALLERY}__load-btn`;
-export const PRODUCT_GALLERY_CONTAINER = `${PRODUCT_GALLERY}__container`;
-export const PRODUCT_GALLERY_ERROR_MSG = `${PRODUCT_GALLERY}__error-message`;
+export const PRODUCT_GALLERY_SWIPER = `${PRODUCT_GALLERY}__swiper`;
+export const PRODUCT_GALLERY_BTN_PREV = `${PRODUCT_GALLERY}__btn-previous`;
+export const PRODUCT_GALLERY_BTN_NEXT = `${PRODUCT_GALLERY}__btn-next`;
 
 const isImgUrl = (path: string): boolean => /\.(jpeg|jpg|gif|png|webp)$/i.test(path);
 const isVideoUrl = (path: string): boolean => /\.mp4$/i.test(path);
-const IMG_BATCH_SIZE = 6;
+const MEDIA_BATCH_SIZE = 6;
 
 const ProductGallery: FC<TProductGalleryProps> = (props) => {
   const { className, appearance } = props;
   const classes = classNames(PRODUCT_GALLERY, className);
-  const [maxImgCount, setMaxImgCount] = useState(IMG_BATCH_SIZE);
+  const [maxMediaCount, setMaxMediaCount] = useState(MEDIA_BATCH_SIZE);
 
   const handleLoadBtnClick = useCallback(() => {
-    setMaxImgCount((prevCount) => prevCount + IMG_BATCH_SIZE);
+    setMaxMediaCount((prevCount) => prevCount + MEDIA_BATCH_SIZE);
   }, []);
 
-  const images = useMemo(
+  const mediaItems = useMemo(
     () =>
-      appearance.mediaURL.slice(0, maxImgCount).map((url) => {
-        const key = JSON.stringify(url);
+      appearance.mediaURL
+        .map((url) => {
+          const key = JSON.stringify(url);
 
-        if (isImgUrl(url)) {
-          return <img key={key} className={PRODUCT_GALLERY_ITEM} src={url} alt="" />;
-        }
+          let media = null;
 
-        if (isVideoUrl(url)) {
+          if (isImgUrl(url)) {
+            media = <img className={PRODUCT_GALLERY_MEDIA} src={url} alt="" />;
+          } else if (isVideoUrl(url)) {
+            media = (
+              <video
+                className={PRODUCT_GALLERY_MEDIA}
+                playsInline
+                loop
+                autoPlay
+                preload="none"
+                muted>
+                <source src={url} type="video/mp4" />
+              </video>
+            );
+          }
+
           return (
-            <video
-              className={PRODUCT_GALLERY_ITEM}
-              key={key}
-              playsInline
-              loop
-              autoPlay
-              preload="none"
-              muted>
-              <source src={url} type="video/mp4" />
-            </video>
+            <div className={PRODUCT_GALLERY_MEDIA_ITEM} key={key}>
+              {media}
+            </div>
           );
-        }
-      }),
-    [appearance, maxImgCount]
+        })
+        .filter((item) => !!item),
+    [appearance]
   );
+
+  const slides = useMemo(
+    () => mediaItems.map((item) => <SwiperSlide key={item.key}>{item}</SwiperSlide>),
+    [mediaItems]
+  );
+
+  const showMedia = useMemo(() => mediaItems.slice(0, maxMediaCount), [mediaItems, maxMediaCount]);
 
   return (
     <div className={classes}>
-      <div className={PRODUCT_GALLERY_GRID}>{images}</div>
-      {maxImgCount < appearance.mediaURL.length && (
+      <div className={PRODUCT_GALLERY_SWIPER}>
+        <Button el="button" theme="transparent" view="round" className={PRODUCT_GALLERY_BTN_PREV}>
+          <IoIosArrowBack />
+        </Button>
+        <Button
+          disabled
+          el="button"
+          theme="transparent"
+          view="round"
+          className={PRODUCT_GALLERY_BTN_NEXT}>
+          <IoIosArrowForward />
+        </Button>
+        <Swiper
+          spaceBetween={10}
+          navigation={{
+            nextEl: `.${PRODUCT_GALLERY_BTN_NEXT}`,
+            prevEl: `.${PRODUCT_GALLERY_BTN_PREV}`,
+          }}
+          modules={[Navigation]}>
+          {slides}
+        </Swiper>
+      </div>
+      <div className={PRODUCT_GALLERY_GRID}>{showMedia}</div>
+      {maxMediaCount < appearance.mediaURL.length && (
         <Button
           el="button"
           className={PRODUCT_GALLERY_LOAD_BTN}
           uppercase
-          view="primary"
+          view="secondary"
           theme="primary"
           onClick={handleLoadBtnClick}>
           Load more <IoIosArrowDown />
--- src/constants/classNames.ts
+++ src/constants/classNames.ts
@@ -2,3 +2,4 @@ export const WRAPPER = 'wrapper';
 export const ICON_MD = 'icon-md';
 export const BG_OBJECT = 'bg-object';
 export const SECTION = 'section';
+export const SWIPER_EQUAL_SLIDES = 'swiper-equal-slides';
